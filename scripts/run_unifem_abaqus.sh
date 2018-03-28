f38dae30c63d33651d47c04b022e9eb72217ea7f
woscj607 <woscj607@163.com>
2018-03-28 10:54:01 +0800

    add single test

--- scripts/run_unifem_abaqus.sh
+++ scripts/run_unifem_abaqus.sh
@@ -6,4 +6,4 @@ export PYTHONPATH=/home/$username/work/gens_libs/meshgen/py/dist/
 export LD_LIBRARY_PATH=/home/$username/work/gens_libs/meshgen/lib/linux64/3rdparty/:/home/$username/work/gens_libs/meshgen/lib/linux64/simright
 export CALCULIX_CCX_EXE=/home/$username/work/ccx/ccx_2.13
 
-/home/jiechen/.pyenv/shims/python /home/$username/work/gens_libs/caegen-node/tests/test_caegen.py
+/home/jiechen/.pyenv/shims/python /home/$username/work/gens_libs/unifem_abaqus/tests/test_abaqus_io.py
